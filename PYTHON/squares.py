903f22732b5eceee802a3e572179476bcd0c0501
Murdock021 <diogomachadocmb@gmail.com>
2024-01-17 07:35:40 -0300

    CS50

---
+++ PYTHON/squares.py
@@ -0,0 +1,13 @@
+from functions import square
+from functions import quadrado
+
+
+for i in range(10):
+    print("{} squared is {}".format(i, square(i)))
+    
+
+#Para melhor intendimento da função 
+
+
+for i in range (10):
+    print("{} ao quadrado é {}".format(i,quadrado(i)))
