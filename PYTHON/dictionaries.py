c5011db0e852d538250825146f942f34ce14dfd1
Murdock021 <diogomachadocmb@gmail.com>
2024-01-17 06:56:25 -0300

    CS50

--- PYTHON/dictionaries.py
+++ PYTHON/dictionaries.py
@@ -1,3 +1,8 @@
+houses={"Harry":"Gryffindor","Draco":"Slytherin"}
+houses ["Diogo"] = "Zadok"
+print(houses["Diogo"])
+
+
 ages = {"Alice": 22, "Bob": 27}
 ages["Charlie"] = 30
 ages["Alice"] += 1
--- PYTHON/loops.py
+++ PYTHON/loops.py
@@ -4,3 +4,7 @@ for i in range(5):
 names = ["Alice", "Bob", "Charlie"]
 for name in names:
     print(name)
+
+list = "Diogo"
+for character in list:
+    print(character)
